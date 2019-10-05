6ebb9178ea2c95d9c89fdb70435ad38cd0e3e72e
CodingRuslan <orion.hola@gmail.com>
2019-10-05 17:22:18 +0300

    feat: add routing and authentication content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MenuListComponent} from "./menu-list/menu-list.component";
+import {HistoryListComponent} from "./history-list/history-list.component";
+import {HistoryListItemDetailsComponent} from "./history-list-item-details/history-list-item-details.component";
+import {RegistrationPageComponent} from "./registration-page/registration-page.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MenuListComponent},
+  {path: 'history', component: HistoryListComponent},
+  {path: 'history/:id', component: HistoryListItemDetailsComponent},
+  {path: 'registration', component: RegistrationPageComponent},
+  {path: 'login', component: LoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
